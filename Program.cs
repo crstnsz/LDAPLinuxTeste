11c0bc402da7a6b8cfa210b636140d8fe3346adc
Cristiano Souza <cristiano.souza@becomex.com.br>
2019-08-23 17:34:32 -0300

    Teste de conexão ao active directory no linux

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Novell.Directory.Ldap;
+
+namespace OpenLdapTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "****";
+            string domainName = "****";
+            string password = "****";
+
+            string userDn = $"{username}@{domainName}";
+            try
+            {
+                using (var connection = new LdapConnection {SecureSocketLayer = false})
+                {
+                    connection.Connect(domainName, LdapConnection.DEFAULT_PORT);
+                    connection.Bind(userDn, password);
+                    if (connection.Bound)
+                        Console.WriteLine("Sucesso");
+                }
+            }
+            catch (LdapException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
